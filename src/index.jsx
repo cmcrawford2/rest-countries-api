70b8fc4b1e672fc4a41f0be508aacc2c987b3ef9
Cris Crawford <69829777+cmcrawford2@users.noreply.github.com>
2023-11-16 11:38:23 -0500

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
